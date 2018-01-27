eb9f772419965a36f5b30a56bd55451c168b8067
Thomas Protheroe <CanadianCavalry@gmail.com>
2018-01-27 09:48:24 -0800

    Added input delay and beat timer. Level segments destroyed when off camera

--- Assets/Scripts/LevelSegment.cs
+++ Assets/Scripts/LevelSegment.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public class LevelSegment : MonoBehaviour {
 
 	public float scrollSpeed;
+	public Camera runnerCam;
+
+	private Collider2D coll;
+	private Plane[] planes;
 
 	// Use this for initialization
 	void Start () {
-		
+		planes = GeometryUtility.CalculateFrustumPlanes (runnerCam);
+		coll = GetComponent<Collider2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-
-	void FixedUpdate() {
 		scroll ();
+
+		if (GeometryUtility.TestPlanesAABB (planes, coll.bounds)) {
+
+		} else {
+			Destroy (gameObject);
+		}
 	}
 
 	private void scroll() {
-		transform.position = new Vector2 (transform.position.x - scrollSpeed, transform.position.y);
+		transform.position = new Vector2 (transform.position.x - (scrollSpeed * Time.deltaTime), transform.position.y);
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,51 +4,74 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	private List<PlayerInput> inputs;
+	private Queue<PlayerInput> inputs;
+	private PlayerInput currentInput;
 	private int maxInputs { get; set; }
+	private float timeToBeat;
+
+	public float beatInterval;
 	public Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-		inputs = new List<PlayerInput> ();
+		inputs = new Queue<PlayerInput> ();
 		maxInputs = 8;
+		timeToBeat = beatInterval;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		timeToBeat -= Time.deltaTime;
 		getPlayerInput ();
+
+		if (timeToBeat <= 0) {
+			if (inputs.Count > 0) {
+				currentInput = inputs.Dequeue ();
+				Invoke (currentInput.inputName, 0.0f);
+			}
+
+			float diff = timeToBeat * -1;
+			timeToBeat = beatInterval + diff;
+		}
+
+		Debug.Log (timeToBeat);
 	}
 
 	private void getPlayerInput() {
 		if (!(inputs.Count >= maxInputs)) {
 			if (Input.GetButtonDown ("jump")) {
-				inputs.Add (new PlayerInput ("jump"));
-				jump ();
+				inputs.Enqueue (new PlayerInput ("jump"));
 			} else if (Input.GetButtonDown ("duck")) {
-				inputs.Add (new PlayerInput ("duck"));
+				inputs.Enqueue (new PlayerInput ("duck"));
 			} else if (Input.GetButtonDown ("longjump")) {
-				inputs.Add (new PlayerInput ("longjump"));
+				inputs.Enqueue (new PlayerInput ("longjump"));
 			} else if (Input.GetButtonDown ("highjump")) {
-				inputs.Add (new PlayerInput ("highjump"));
+				inputs.Enqueue (new PlayerInput ("highjump"));
 			}
 		}
 	}
 
 	private void jump() {
-		//play jump animation
+		//TODO play animation
 
 		rb.AddForce (new Vector2(0.0f, 200.0f));
 	}
 
 	private void duck() {
+		//TODO play animation
 
+		//TODO reduce hitbox height
 	}
 
 	private void longJump() {
+		//TODO play animation
+
 		rb.AddForce (new Vector2(0.0f, 300.0f));
 	}
 
 	private void highJump() {
+		//TODO play animation
 
+		rb.AddForce (new Vector2(0.0f, 300.0f));
 	}
 }
